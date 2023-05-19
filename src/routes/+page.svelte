0d56d258e2cd83587e59fc4fd114a57b82283062
tomas-b <tomas.b.lp@gmail.com>
2023-05-19 19:48:37 -0300

    sveltefire base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,17 @@
-<h1>Hello world</h1>
+<script>
+  import { auth } from "$lib/firebase";
+  import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+
+  import { userStore } from "sveltefire";
+  const user = userStore(auth);
+</script>
+
+{#if $user}
+  <h1>Welcome, {$user.displayName}</h1>
+  <button on:click={() => signOut(auth)}> Sign Out </button>
+{:else}
+  <h1>Not logged in</h1>
+  <button on:click={() => signInWithPopup(auth, new GoogleAuthProvider())}>
+    Sign In With Google
+  </button>
+{/if}
